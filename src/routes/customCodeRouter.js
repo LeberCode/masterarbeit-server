fe610344a80c5f141b3207500b54b5680d0f96c8
Robin Leber <rleber98@gmail.com>
2024-06-21 18:22:01 +0200

    FIX: clear Model

--- src/routes/customCodeRouter.js
+++ src/routes/customCodeRouter.js
@@ -1,14 +1,29 @@
 const express = require("express");
-const { addCustomCodeToJson } = require("../functions/helperFunctions");
+const {
+  addCustomCodeToJson,
+  emptyCustomCodeDatabase,
+} = require("../functions/helperFunctions");
 const customCodeRouter = express.Router();
 
 customCodeRouter.post("/", async (req, res) => {
   try {
     addCustomCodeToJson(req.body);
-    res.status(200).json({ message: "Custom Code erfolgreich hinzugefügt" });
+    res.status(200).json({ message: "Custom Code erfolgreich hinzugefügt!" });
   } catch (e) {
     console.error(e);
     res.status(500).json({ message: "Es ist etwas schief gelaufen!" });
+  }
+});
+
+customCodeRouter.get("/clear", async (req, res) => {
+  try {
+    emptyCustomCodeDatabase();
+    res.status(200).json({ message: "Custom Code JSON erfolgreich geleert!" });
+  } catch (e) {
+    console.error(e);
+    res
+      .status(500)
+      .json({ message: "Custom Code JSON leeren fehlgeschlagen!" });
   }
 });
 
